13f45075a8c3d2ab94e06335b120679e453ff5fc
Jaemin Han <j4emin.han@gmail.com>
2021-07-17 22:39:09 -0400

    complete text & image structuring

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,14 +1,36 @@
 import {
   Box,
   Stack,
-  Center
+  Center,
+  Text,
+  Flex,
+  Image
 } from '@chakra-ui/react';
 
 function Container() {
   return (
-    <Stack rounded="3xl" h="50vh" maxW="90%" m="auto" direction="row" shadow="lg">
-      <Box w="25%" bg="red" roundedLeft="3xl"></Box>
-      <Box w = "75%" bg="blue" roundedRight="3xl">2</Box>
+    <Stack rounded="2xl" h="50vh" maxW="90%" m="auto" direction="row" shadow="lg">
+      <Flex padding="5vh" w="25%" roundedLeft="2xl" backgroundImage="bg4.jpg">
+        <Center>
+          <Stack color="white" fontWeight="400">
+            <Image src='Logo.png' rounded="md" boxSize="12vh"></Image>
+            <Box>
+              <Text fontSize="25">jaemin han</Text>
+            </Box>
+            <Box>
+              <Text fontSize='lg'>fullstack web developer</Text>
+            </Box>
+            <Box>
+              <Text>github</Text>
+              <Text>linkedin</Text>
+              <Text>discord</Text>
+              <Text>email</Text>
+              <Text>location</Text>
+            </Box>
+          </Stack>
+        </Center>
+      </Flex>
+      <Box w = "75%" bg="blue" roundedRight="2xl">2</Box>
     </Stack>
   );
 }
