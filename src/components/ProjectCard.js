c49b1965cdb99722cce9beea28f7f303f6f19f19
Jaemin Han <j4emin.han@gmail.com>
2021-07-18 01:41:48 -0400

    update styling & hover properties on cards

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,9 +3,9 @@ import { Box, Center, Stack, HStack, Text, Flex} from '@chakra-ui/react'
 import { FaCode } from 'react-icons/fa'
 
 function ProjectCard(props) {
-    const { name, desc, tech } = props
+    const { name, desc, tech, link } = props
     return (
-        <Box rounded="md" w="49%" minH="16.8vh" shadow="md" padding="3vh">
+        <Box onClick={() => {window.open(link)}} _hover={{bg: "gray.50", cursor: "pointer", transition: "all .2s ease-in-out", transform: 'scale(1.015)'}} rounded="md" w="49%" minH="16.8vh" shadow="md" padding="3vh">
             <Flex direction="column" h="100%" justify="space-around">
                 <Text fontSize="5xl" fontWeight="600">{name}</Text>
                 <Text fontSize="xl">{desc}</Text>
