cf161d043e27e70f314aaa61241bda055b99401b
Jaemin Han <j4emin.han@gmail.com>
2021-07-18 00:42:36 -0400

    complete individual box prop passing & styling

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,15 +1,16 @@
 import React from 'react'
-import { Box, Center, Stack, HStack, Text} from '@chakra-ui/react'
+import { Box, Center, Stack, HStack, Text, Flex} from '@chakra-ui/react'
+import { FaCode } from 'react-icons/fa'
 
 function ProjectCard(props) {
     const { name, desc, tech } = props
     return (
-        <Box rounded="md" w="49%" minH="16.8vh" shadow="md" padding="5vh">
-            <Stack>
-                <Text>{name}</Text>
-                <Text>{desc}</Text>
-                <HStack>{tech}</HStack>
-            </Stack>
+        <Box rounded="md" w="49%" minH="16.8vh" shadow="md" padding="3vh">
+            <Flex direction="column" h="100%" justify="space-around">
+                <Text fontSize="2xl" fontWeight="600">{name}</Text>
+                <Text fontSize="xl">{desc}</Text>
+                <HStack fontSize="xl" fontWeight="600"><FaCode/><Text>{tech}</Text></HStack>
+            </Flex>
         </Box>
     )
 }
