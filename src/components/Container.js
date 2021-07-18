ff4d300aa91065ed7d04fe977dbe9ba68c390733
Jaemin Han <j4emin.han@gmail.com>
2021-07-17 23:25:38 -0400

    finish personal details sidebar & populate text with hrefs

--- src/components/Container.js
+++ src/components/Container.js
@@ -4,33 +4,48 @@ import {
   Center,
   Text,
   Flex,
-  Image
+  Image,
+  Link,
+  HStack
 } from '@chakra-ui/react';
+
+import { AiFillGithub, AiFillLinkedin, AiOutlineMail } from 'react-icons/ai'
+import { FaDiscord } from 'react-icons/fa'
+import { ImLocation } from 'react-icons/im'
 
 function Container() {
   return (
-    <Stack rounded="2xl" h="50vh" maxW="90%" m="auto" direction="row" shadow="lg">
+    <Stack rounded="2xl" h="50vh" maxW="90%" m="auto" direction="row" spacing='0' shadow="lg">
       <Flex padding="5vh" w="25%" roundedLeft="2xl" backgroundImage="bg4.jpg">
         <Center>
           <Stack color="white" fontWeight="400">
             <Image src='Logo.png' rounded="md" boxSize="12vh"></Image>
-            <Box>
+            <Stack spacing="-0.5vh">
               <Text fontSize="25">jaemin han</Text>
-            </Box>
+              <Text>@poof</Text>
+            </Stack>
             <Box>
-              <Text fontSize='lg'>fullstack web developer</Text>
+              <Text fontSize='xl'>fullstack web developer</Text>
             </Box>
+            <Stack spacing="1px">
+              <HStack spacing='5px'><AiFillGithub/><Link href="https://github.com/xPuffball">github</Link></HStack>
+              <HStack spacing='5px'><AiFillLinkedin/><Link href="https://www.linkedin.com/in/hanjaemin/">linkedin</Link></HStack>
+              <HStack spacing='5px'><FaDiscord/><Text>poof#5402</Text></HStack>
+              <HStack spacing='5px'><AiOutlineMail/><Text>j4emin.han@gmail.com</Text></HStack>
+            </Stack>
             <Box>
-              <Text>github</Text>
-              <Text>linkedin</Text>
-              <Text>discord</Text>
-              <Text>email</Text>
-              <Text>location</Text>
+              <HStack><ImLocation/><Text>toronto</Text></HStack>
             </Box>
           </Stack>
         </Center>
       </Flex>
-      <Box w = "75%" bg="blue" roundedRight="2xl">2</Box>
+      <Box w = "75%" bg="white" roundedRight="2xl" padding="5vh">
+        <Stack>
+          <Text fontSize="50" fontWeight="900">
+            my work.
+          </Text>
+        </Stack>
+      </Box>
     </Stack>
   );
 }
