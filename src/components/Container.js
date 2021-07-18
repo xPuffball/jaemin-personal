cba1b6f0841132288ee7ae074c39243f48526f58
Jaemin Han <j4emin.han@gmail.com>
2021-07-18 00:28:10 -0400

    add scrollable box interface under my works

--- src/components/Container.js
+++ src/components/Container.js
@@ -9,14 +9,16 @@ import {
   HStack
 } from '@chakra-ui/react';
 
+import ProjectCard from './ProjectCard'
+
 import { AiFillGithub, AiFillLinkedin, AiOutlineMail } from 'react-icons/ai'
 import { FaDiscord } from 'react-icons/fa'
 import { ImLocation } from 'react-icons/im'
 
 function Container() {
   return (
-    <Stack rounded="2xl" h="50vh" maxW="90%" m="auto" direction="row" spacing='0' shadow="lg">
-      <Flex padding="5vh" w="25%" roundedLeft="2xl" backgroundImage="bg4.jpg">
+    <Stack rounded="2xl" h="50vh" maxW="80%" m="auto" direction="row" spacing='0' shadow="lg">
+      <Flex padding="5vh" w="25vh" roundedLeft="2xl" backgroundImage="bg4.jpg">
         <Center>
           <Stack color="white" fontWeight="400">
             <Image src='Logo.png' rounded="md" boxSize="12vh"></Image>
@@ -39,11 +41,35 @@ function Container() {
           </Stack>
         </Center>
       </Flex>
-      <Box w = "75%" bg="white" roundedRight="2xl" padding="5vh">
-        <Stack>
-          <Text fontSize="50" fontWeight="900">
-            my work.
-          </Text>
+      <Box w = "75vw" bg="white" roundedRight="2xl" paddingLeft="75px" paddingRight="75px" paddingTop="50px" paddingBottom="50px">
+        <Text fontSize="50" fontWeight="900">
+          my work.
+        </Text>
+        <Stack 
+          h="90%" 
+          overflowY="scroll"
+          css={{
+            '&::-webkit-scrollbar': {
+              width: '0px',
+            }
+          }}
+        >
+          <Flex justify="space-between" mb="18px">
+            <ProjectCard name="HobbyNet" desc="hello bingo bongo" tech="react"></ProjectCard>
+            <ProjectCard></ProjectCard>
+          </Flex>
+          <Flex justify="space-between">
+            <ProjectCard></ProjectCard>
+            <ProjectCard></ProjectCard>
+          </Flex>
+          <Flex justify="space-between">
+            <ProjectCard></ProjectCard>
+            <ProjectCard></ProjectCard>
+          </Flex>
+          <Flex justify="space-between">
+            <ProjectCard></ProjectCard>
+            <ProjectCard></ProjectCard>
+          </Flex>
         </Stack>
       </Box>
     </Stack>
